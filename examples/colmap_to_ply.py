d1a9c9d76fc004fe51b42bbf0b4a3bbb16ee3b69
nburgdorfer <nburgdorfer@gmail.com>
2024-07-10 17:59:05 -0400

    updates

---
+++ examples/colmap_to_ply.py
@@ -0,0 +1,43 @@
+import sys, os
+import numpy as np
+
+
+
+
+def load_points(points_file, output_file):
+    points = []
+    with open(points_file, 'r') as pf:
+        lines = pf.readlines()[3:]
+
+        for i, l in enumerate(lines):
+            points.append(np.asarray(l.strip().split()[1:7]))
+
+    # write header meta-data
+    ply_str = ""
+    ply_str += "ply\n"
+    ply_str += "format ascii 1.0\n"
+    ply_str += "comment Right-Handed System\n"
+    ply_str += f"element vertex {len(points)}\n"
+    ply_str += "property float x\n"
+    ply_str += "property float y\n"
+    ply_str += "property float z\n"
+    ply_str += "property uchar red\n"
+    ply_str += "property uchar green\n"
+    ply_str += "property uchar blue\n"
+    ply_str += "end_header\n"
+
+    for point in points:
+        ply_str += f"{point[0]} {point[1]} {point[2]} {point[3]} {point[4]} {point[5]}\n"
+
+    with open(output_file, 'w') as of:
+        of.write(ply_str)
+
+
+def main():
+    points_file = sys.argv[1]
+    output_file = sys.argv[2]
+    points = load_points(points_file, output_file)
+
+
+if __name__=="__main__":
+    main()
