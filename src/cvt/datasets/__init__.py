cbb34fe9510a8b0abc6a8174bea52914ee87847d
nburgdorfer <nburgdorfer@gmail.com>
2024-12-06 14:28:56 -0500

    updates

--- src/cvt/datasets/__init__.py
+++ src/cvt/datasets/__init__.py
@@ -5,8 +5,6 @@
 This package includes the following modules:
 
 - `dtu`: A collection of functions for the DTU dataset.
-- `tanks`: A collection of functions for the Tanks and Temples dataset.
-- `blended`: A collection of functions for the BlendedMVS(MVG) dataset.
 """
 import os
 import sys
@@ -16,6 +14,4 @@ PYTHON_ROOT=os.path.abspath(os.path.dirname(FILE_ROOT))
 
 sys.path.append(PYTHON_ROOT)
 
-from .dtu import *
-from .tanks import *
-from .blended import *
+from .DTU import *
